35d9a0811ab8a57f8062fe60b1d767fecf3a0b66
vinhnx <nxvinh222@gmail.com>
2021-10-01 21:51:48 +0700

    Version 1.0

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ try {
     client.on('ready', () => {
         console.log("ready!!!")
 
-        const guideId = '884098897377636452'
+        const guideId = '886977856788393987'
         const guild = client.guilds.cache.get(guideId)
         let commands
 
@@ -81,28 +81,33 @@ try {
         })
 
         commands?.create({
-            name: 'attack-draw',
-            description: 'Bên tấn công rút bài'
+            name: 'game-manual',
+            description: 'Hướng dẫn chơi'
         })
 
         commands?.create({
-            name: 'defend-draw',
-            description: 'Bên phòng thủ rút bài'
+            name: 'p1-draw',
+            description: 'Player 1 rút bài'
         })
 
         commands?.create({
-            name: 'attack-drawbottom',
-            description: 'Bên tấn công rút lá bài ở cuối bộ bài'
+            name: 'p2-draw',
+            description: 'Player 2 rút bài'
         })
 
         commands?.create({
-            name: 'defend-drawbottom',
-            description: 'Bên phòng thủ rút lá bài ở cuối bộ bài'
+            name: 'p1-drawbottom',
+            description: 'Player 1 rút lá ở cuối bộ bài lên tay'
         })
 
         commands?.create({
-            name: 'attack-insert',
-            description: 'Bên tấn công nhét 1 lá bài vào bộ bài',
+            name: 'p2-drawbottom',
+            description: 'Player 2 rút lá ở cuối bộ bài lên tay'
+        })
+
+        commands?.create({
+            name: 'p1-insert',
+            description: 'Player 1 nhét một lá bài vào bộ bài',
             options: [
                 {
                     name: 'number',
@@ -114,8 +119,34 @@ try {
         })
 
         commands?.create({
-            name: 'defend-insert',
-            description: 'Bên phòng thủ nhét 1 lá bài vào bộ bài',
+            name: 'p2-insert',
+            description: 'Player 2 nhét một lá bài vào bộ bài',
+            options: [
+                {
+                    name: 'number',
+                    description: 'số của lá bài',
+                    required: true,
+                    type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+                },
+            ]
+        })
+
+        commands?.create({
+            name: 'p1-choose',
+            description: 'Player 1 chọn một lá trong bộ bài và đưa lên tay',
+            options: [
+                {
+                    name: 'number',
+                    description: 'số của lá bài',
+                    required: true,
+                    type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+                },
+            ]
+        })
+
+        commands?.create({
+            name: 'p2-choose',
+            description: 'Player 2 chọn một lá trong bộ bài và đưa lên tay',
             options: [
                 {
                     name: 'number',
@@ -139,11 +170,17 @@ try {
             return
         }
 
+        if (![channel1, channel2, channel3].includes(interaction.channel.id)) {
+            interaction.reply({
+                content: `Xin hãy chơi ở trong channel game!`,
+            })
+            return
+        }
+
         const { commandName, options } = interaction
         let arr = get_arr(interaction.channel.id)
         let atk = arr.atk
         let def = arr.def
-
 
         switch (commandName) {
             case 'game-start':
@@ -166,8 +203,30 @@ try {
                 // })
                 interaction.reply({
                     content: `Bốc bài!\n` +
-                        `Bên tấn công: [${draw_atk.join('] [')}]\n` +
-                        `Bên phòng thủ: [${draw_def.join('] [')}]`,
+                        `P1--> [${draw_atk.join('] [')}]\n` +
+                        `-------------------------------\n` +
+                        `P2--> [${draw_def.join('] [')}]`,
+                })
+
+                break;
+
+            case 'game-manual':
+                interaction.reply({
+                    content: "Hướng dẫn sử dụng bot chia bài:\n" +
+                        "`\\game-manual`: xem hướng dẫn chơi\n" +
+                        "`\\game-start`: bắt đầu ván chơi mới\n" +
+                        "---------------------------\n" +
+                        "`\\p1-draw`: Player 1 rút bài\n" +
+                        "`\\p2-draw`: Player 2 rút bài\n" +
+                        "---------------------------\n" +
+                        "`\\p1-drawbottom`: Player 1 rút lá ở cuối bộ bài lên tay\n" +
+                        "`\\p2-drawbottom`: Player 2 rút lá ở cuối bộ bài lên tay\n" +
+                        "---------------------------\n" +
+                        "`\\p1-insert number`: Player 1 nhét lá bài [number] vào bộ bài\n" +
+                        "`\\p2-insert number`: Player 2 nhét lá bài [number] vào bộ bài\n" +
+                        "---------------------------\n" +
+                        "`\\p1-choose number`: Player 1 chọn lá bài [number] và đua lên tay\n" +
+                        "`\\p2-choose number`: Player 2 chọn lá bài [number] và đua lên tay\n",
                 })
 
                 break;
@@ -179,10 +238,10 @@ try {
 
                 break;
 
-            case 'attack-draw':
+            case 'p1-draw':
                 if (atk.length == 0) {
                     interaction.reply({
-                        content: `Hết bài, bên phòng thủ chiến thắng!`,
+                        content: `Hết bài, Player 2 chiến thắng!`,
                     })
                     break;
                 }
@@ -198,15 +257,15 @@ try {
                 //     `,
                 // })
                 interaction.reply({
-                    content: `[${draw_atk}]\nBên tấn công rút bài`,
+                    content: `Player 1 rút bài\n[${draw_atk}]`,
                 })
 
                 break;
 
-            case 'attack-drawbottom':
+            case 'p1-drawbottom':
                 if (atk.length == 0) {
                     interaction.reply({
-                        content: `Hết bài, bên phòng thủ chiến thắng!`,
+                        content: `Hết bài, Player 2 chiến thắng!`,
                     })
                     break;
                 }
@@ -221,15 +280,15 @@ try {
                 //         `,
                 // })
                 interaction.reply({
-                    content: `[${draw_atk}]\nBên tấn công rút bài`,
+                    content: `Player 1 rút bài ở cuối\n[${draw_atk}]`,
                 })
 
                 break;
 
-            case 'defend-draw':
+            case 'p2-draw':
                 if (def.length == 0) {
                     interaction.reply({
-                        content: `Hết bài, bên tấn công chiến thắng!`,
+                        content: `Hết bài, Player 1 chiến thắng!`,
                     })
                     break;
                 }
@@ -245,15 +304,15 @@ try {
                 //         `,
                 // })
                 interaction.reply({
-                    content: `[${draw_def}]\nBên phòng thủ rút bài`,
+                    content: `Player 2 rút bài\n[${draw_def}]`,
                 })
 
                 break;
 
-            case 'defend-drawbottom':
+            case 'p2-drawbottom':
                 if (def.length == 0) {
                     interaction.reply({
-                        content: `Hết bài, bên tấn công chiến thắng!`,
+                        content: `Hết bài, Player 1 chiến thắng!`,
                     })
                     break;
                 }
@@ -268,12 +327,12 @@ try {
                 //             `,
                 // })
                 interaction.reply({
-                    content: `[${draw_def}]\nBên phòng thủ rút lá cuối cùng của bộ bài`,
+                    content: `Player 2 rút bài ở cuối\n[${draw_def}]`,
                 })
 
                 break;
 
-            case 'attack-insert':
+            case 'p1-insert':
                 number = options.getNumber('number') || -1
 
                 if (number > deck_size || number < 0) {
@@ -302,12 +361,12 @@ try {
                 //     // ephemeral: true,
                 // })
                 await interaction.editReply({
-                    content: `Đã đưa lá bài [${number}] vào bộ bài`,
+                    content: `Player 1 đưa lá bài [${number}] vào bộ bài`,
                     // ephemeral: true,
                 })
                 break;
 
-            case 'defend-insert':
+            case 'p2-insert':
                 number = options.getNumber('number') || -1
 
                 if (number > deck_size || number < 0) {
@@ -336,7 +395,43 @@ try {
                 //     // ephemeral: true,
                 // })
                 await interaction.editReply({
-                    content: `Đã đưa lá bài [${number}] vào bộ bài`,
+                    content: `Player 2 đưa lá bài [${number}] vào bộ bài`,
+                    // ephemeral: true,
+                })
+                break;
+
+            case 'p1-choose':
+                number = options.getNumber('number') || -1
+
+                if (atk.indexOf(number) == -1) {
+                    interaction.reply({
+                        content: `Lá bài [${number}] không ở trong bộ bài!`,
+                        // ephemeral: true,
+                    })
+                    break;
+                }
+
+                atk.splice(atk.indexOf(number), 1);
+                interaction.reply({
+                    content: `Player 1 lấy lá bài [${number}] lên tay`,
+                    // ephemeral: true,
+                })
+                break;
+
+            case 'p2-choose':
+                number = options.getNumber('number') || -1
+
+                if (def.indexOf(number) == -1) {
+                    interaction.reply({
+                        content: `Lá bài [${number}] không ở trong bộ bài!`,
+                        // ephemeral: true,
+                    })
+                    break;
+                }
+
+                def.splice(def.indexOf(number), 1);
+                interaction.reply({
+                    content: `Player 2 lấy lá bài [${number}] lên tay`,
                     // ephemeral: true,
                 })
                 break;
